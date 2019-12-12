cc1a9e9c66ef96b6164bbbe122465f63686e429a
ataft <11670864+ataft@users.noreply.github.com>
2019-12-11 21:45:47 -0800

    Deploy website version based on 698feee202dd7ed81fc47a8b5ad06db11329b4bd

---
+++ 1ca9f356.047792da.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{102:function(e){e.exports=JSON.parse('{"id":"report","title":"Reporting User Guide","hide_title":true,"sidebar_label":"Reports","description":"# Reporting User Guide\\r","source":"@site/docs\\\\report.md","permalink":"/docs/report","editUrl":"https://github.com/ataft/flexit-docs/edit/master/docs/report.md"}')}}]);
