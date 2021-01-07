5a725b7db914fd39955d9cce65a8d981ad0639b2
ataft <11670864+ataft@users.noreply.github.com>
2021-01-07 10:56:47 -0800

    Fix "HeroBtn"

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,8 +75,8 @@ function Home() {
           <div className={styles.buttons}>
             <Link
               className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                'button button--outline button--lg',
+                styles.heroBtn,
               )}
               to={useBaseUrl('docs/getting-started')}>
               Get Started
